369a8f4c755a11b9950d6f91572bc35cd8129194
Hongmiao Yu <hy1709@nyu.edu>
2022-03-07 20:50:40 -0800

    fix monitor bug

--- checkMonitor.py
+++ checkMonitor.py
@@ -1,19 +1,27 @@
 import socket
 import argparse
+from prettytable import PrettyTable
 
 parser = argparse.ArgumentParser(description='check the runs and workers of them')
-parser.add_argument('--host',   type=float, help='monitor address',    default='127.0.0.1')
-parser.add_argument('--port',   type=float, help='monitor port',    default=8080)
+parser.add_argument('--host',   type=str, help='monitor address',    default='127.0.0.1')
+parser.add_argument('--port',   type=int, help='monitor port',    default=8080)
 
 args = parser.parse_args()
 
 client_socket = socket.socket()  # instantiate
 client_socket.connect((args.host, args.port))  # connect to the server
-message = 'create!0!0!0!0\n'
+message = 'show!0!0!0!0\n'
 client_socket.send(message.encode())
-client_socket.close()
-data = client_socket.recv(1024).decode()
+sockFile = client_socket.makefile()
+data = '-1'
+x = PrettyTable()
+x.field_names = ["runId", "min_budget", "max_budget", "worker"]
 while data:  # receive response
-    data = client_socket.recv(1024).decode()
-    print(data)
+    data = sockFile.readline()
+    run = data.split('!')
+    if len(run) is not 4:
+        break
+    x.add_row([run[0], run[1], run[2], run[3]])
 
+print(x)
+client_socket.close()
